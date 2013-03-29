00f8a2846f10796385a3260cbb58d07b043a3bf7
sazonenko <jedy@fccland.ru>
2013-03-29 18:44:27 +0700

    wf with NoWait with arbiter

--- wf-recipes/src/main/java/ru/taskurotta/recipes/nowait/decider/NoWaitDeciderImpl.java
+++ wf-recipes/src/main/java/ru/taskurotta/recipes/nowait/decider/NoWaitDeciderImpl.java
@@ -7,6 +7,7 @@ import ru.taskurotta.annotation.Asynchronous;
 import ru.taskurotta.annotation.NoWait;
 import ru.taskurotta.core.Promise;
 import ru.taskurotta.recipes.nowait.workers.FastWorkerClient;
+import ru.taskurotta.test.FlowArbiter;
 
 /**
  * Created by void 27.03.13 17:11
@@ -14,12 +15,14 @@ import ru.taskurotta.recipes.nowait.workers.FastWorkerClient;
 public class NoWaitDeciderImpl implements NoWaitDecider {
 	protected final static Logger log = LoggerFactory.getLogger(NoWaitDeciderImpl.class);
 
+	private FlowArbiter arbiter;
 	private FastWorkerClient worker;
 	private NoWaitDeciderImpl async;
 
 	@Override
 	public void start() {
 		log.info("start");
+		arbiter.notify("start");
 		Promise<Integer> pB = worker.taskB();
 		Promise<Integer> pC = worker.taskC();
 		async.process(pB, pC);
@@ -28,6 +31,7 @@ public class NoWaitDeciderImpl implements NoWaitDecider {
 	@Asynchronous
 	public Promise<Integer> process(@NoWait Promise<Integer> b, Promise<Integer> c) {
 		log.info("process({}, {})", b, c.get());
+		arbiter.notify("process");
 		return worker.taskE(b.get());
 	}
 
@@ -37,5 +41,9 @@ public class NoWaitDeciderImpl implements NoWaitDecider {
 
 	public void setAsync(NoWaitDeciderImpl async) {
 		this.async = async;
+	}
+
+	public void setArbiter(FlowArbiter arbiter) {
+		this.arbiter = arbiter;
 	}
 }
--- wf-recipes/src/main/java/ru/taskurotta/recipes/nowait/workers/FastWorkerImpl.java
+++ wf-recipes/src/main/java/ru/taskurotta/recipes/nowait/workers/FastWorkerImpl.java
@@ -2,6 +2,7 @@ package ru.taskurotta.recipes.nowait.workers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.taskurotta.test.FlowArbiter;
 
 /**
  * Created by void 27.03.13 19:40
@@ -9,21 +10,33 @@ import org.slf4j.LoggerFactory;
 public class FastWorkerImpl implements FastWorker {
 	protected final static Logger log = LoggerFactory.getLogger(FastWorkerImpl.class);
 
+	private FlowArbiter arbiter;
+
 	@Override
 	public int taskB() {
 		log.info("taskB()");
+
+		arbiter.notify("taskB");
+
+		log.info("taskB done");
 		return 1;
 	}
 
 	@Override
 	public int taskC() {
 		log.info("taskC()");
+		arbiter.notify("taskC");
 		return 2;
 	}
 
 	@Override
 	public int taskE(int b) {
 		log.info("taskE({})", b);
+		arbiter.notify("taskE");
 		return 3;
+	}
+
+	public void setArbiter(FlowArbiter arbiter) {
+		this.arbiter = arbiter;
 	}
 }
