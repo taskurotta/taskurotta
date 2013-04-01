2259f0aebdb4f4a070f9dd138597716356376333
stukushin <stukushin@gmail.com>
2013-04-01 19:39:37 +0700

    Add tests for wf-bootstrap.

---
+++ wf-bootstrap/src/test/java/ru/taskurotta/bootstrap/TestWorkerClient.java
@@ -0,0 +1,13 @@
+package ru.taskurotta.bootstrap;
+
+import ru.taskurotta.annotation.WorkerClient;
+
+/**
+ * User: stukushin
+ * Date: 01.04.13
+ * Time: 18:32
+ */
+@WorkerClient(worker = TestWorker.class)
+public interface TestWorkerClient {
+
+}
