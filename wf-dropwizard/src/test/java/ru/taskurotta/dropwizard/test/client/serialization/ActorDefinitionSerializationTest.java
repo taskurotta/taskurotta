ecdaf1540ddaebb4ed2a882165f1a4531982d469
dudin <Dmitry.Dudin@nsk.fccland.ru>
2013-04-02 14:59:58 +0700

    JSON serialization test added

---
+++ wf-dropwizard/src/test/java/ru/taskurotta/dropwizard/test/client/serialization/ActorDefinitionSerializationTest.java
@@ -0,0 +1,56 @@
+package ru.taskurotta.dropwizard.test.client.serialization;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ru.taskurotta.dropwizard.client.serialization.wrapper.ActorDefinitionWrapper;
+import ru.taskurotta.util.ActorDefinition;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ActorDefinitionSerializationTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ActorDefinitionSerializationTest.class);
+	
+	@Rule
+	public TemporaryFolder tmpFolder = new TemporaryFolder();
+	
+	@Test
+	public void testActorDefinitionSerialization() {
+		ActorDefinition original = createActorDefinition();
+		
+		ObjectMapper jacksonMapper = new ObjectMapper();
+		
+		ActorDefinitionWrapper resultWrapper = null;
+		try {
+			File tmpJsonFile = tmpFolder.newFile();
+			jacksonMapper.writeValue(tmpJsonFile, new ActorDefinitionWrapper(original));
+			resultWrapper = jacksonMapper.readValue(tmpJsonFile, ActorDefinitionWrapper.class);
+		} catch (Exception e) {
+			logger.error("Exception at (de)serialization of ActorDefinitionWrapper to tmp File", e);
+			Assert.fail("Exception at (de)serialization of ActorDefinitionWrapper to tmp File");
+		}
+		
+		Assert.assertNotNull(resultWrapper);
+		if(resultWrapper != null) {
+			ActorDefinition validating = resultWrapper.getActorDefinition();
+			Assert.assertNotNull(validating);
+			if(validating!=null) {
+				Assert.
+			}
+		}
+		
+
+	}
+	
+	private ActorDefinition createActorDefinition() {
+		return ActorDefinition.valueOf("test.me.worker", "7.6.5");
+	}
+	
+}
