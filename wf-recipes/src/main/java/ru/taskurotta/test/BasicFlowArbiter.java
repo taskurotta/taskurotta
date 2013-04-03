06afd330ca423f6e416f4f67d22f43b2fb4972d2
sazonenko <jedy@fccland.ru>
2013-04-03 19:02:01 +0700

    Bootstrap test with peer tasks

--- wf-recipes/src/main/java/ru/taskurotta/test/BasicFlowArbiter.java
+++ wf-recipes/src/main/java/ru/taskurotta/test/BasicFlowArbiter.java
@@ -3,6 +3,7 @@ package ru.taskurotta.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,33 +12,44 @@ import java.util.List;
 public abstract class BasicFlowArbiter implements FlowArbiter {
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	private final List<String> stages;
+	private final List<Stage> stages;
 
 	private String lastTag;
 
 	public BasicFlowArbiter(List<String> stages) {
-		this.stages = stages;
+		this.stages = new LinkedList<Stage>();
+		for (String line : stages) {
+			this.stages.add(new Stage(line));
+		}
 	}
 
 	@Override
 	public void notify(String tag) {
-		log.debug("notified about tag [{}]; {}", tag, stages);
+		log.debug("notified about tag [{}]; Stage list: {}", tag, stages);
 
 		synchronized (stages) {
-			String current = stages.get(0);
-			if (current.equals(tag)) {
-				lastTag = stages.remove(0);
-			} else {
-				throw new IncorrectFlowException("Wrong tag: expected ["+ current +"] but found ["+ tag +"]");
+			Stage current = stages.get(0);
+
+			if (current == null) {
+				throw new IncorrectFlowException("Expected flow finished. Called with " + tag);
+			}
+
+			if (!current.remove(tag)) {
+				throw new IncorrectFlowException("Wrong tag: expected "+ current +" but found {"+ tag +"}");
+			}
+			if (current.isEmpty()) {
+				stages.remove(0);
 			}
 
 			process(tag);
+
+			lastTag = tag;   // saved for waitForTag processes
 			stages.notifyAll();
 		}
 
 	}
 
-	public String getCurrentStage() {
+	public Stage getCurrentStage() {
 		return stages.get(0);
 	}
 
@@ -47,6 +59,9 @@ public abstract class BasicFlowArbiter implements FlowArbiter {
 			while (!lastTag.equals(tag) && timeToWait > 0) {
 				stages.wait(timeToWait);
 				timeToWait = endTime - System.currentTimeMillis();
+			}
+			if (timeToWait <= 0) {
+				throw new IncorrectFlowException("Tag "+ tag +" doesn't checked");
 			}
 		} catch (InterruptedException e) {
 			// just go away
@@ -63,6 +78,8 @@ public abstract class BasicFlowArbiter implements FlowArbiter {
 
 					stages.wait(timeToWait);
 					timeToWait = endTime - System.currentTimeMillis();
+
+					log.debug("Stages: {}, time to wait: {}", stages, timeToWait);
 				}
 			} catch (InterruptedException e) {
 				// just go away
@@ -70,4 +87,10 @@ public abstract class BasicFlowArbiter implements FlowArbiter {
 			return stages.size() == 0;
 		}
 	}
+
+	private boolean checkTag(String tag) {
+		Stage stage = stages.get(0);
+		return stage != null && stage.contains(tag);
+	}
+
 }
