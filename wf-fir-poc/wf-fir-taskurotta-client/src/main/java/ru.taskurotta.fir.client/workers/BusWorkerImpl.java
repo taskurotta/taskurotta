60a4c8445c08240636e9120696ed41979724a571
stukushin <stukushin@gmail.com>
2013-04-23 17:06:14 +0700

    Add JmsTemplate support and sending messages.

--- wf-fir-poc/wf-fir-taskurotta-client/src/main/java/ru.taskurotta.fir.client/workers/BusWorkerImpl.java
+++ wf-fir-poc/wf-fir-taskurotta-client/src/main/java/ru.taskurotta.fir.client/workers/BusWorkerImpl.java
@@ -1,5 +1,13 @@
 package ru.taskurotta.fir.client.workers;
 
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 /**
  * User: stukushin
  * Date: 23.04.13
@@ -7,10 +15,22 @@ package ru.taskurotta.fir.client.workers;
  */
 public class BusWorkerImpl implements BusWorker {
 
-    //private JmsTemplate jmsTemplate;
+    private JmsTemplate jmsTemplate;
 
     @Override
-    public void sendPackage(String uuid, int result) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void sendPackage(final String uuid, final int result) {
+        jmsTemplate.send(new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage message = session.createTextMessage();
+                message.setText(String.valueOf(result));
+                message.setStringProperty("FCCNumber", uuid);
+                return message;
+            }
+        });
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
     }
 }
