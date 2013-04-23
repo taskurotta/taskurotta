b83c214f37f9d996172f4d84a70c5b323d0bd484
Grigorii Tkachuk <tkgreg@me.com>
2013-04-23 15:15:29 +0900

    another quick fix

--- wf-backend-ora/src/test/java/ru/taskurotta/backend/ora/runnable/CreationTask.java
+++ wf-backend-ora/src/test/java/ru/taskurotta/backend/ora/runnable/CreationTask.java
@@ -1,12 +1,11 @@
 package ru.taskurotta.backend.ora.runnable;
 
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.UUID;
-
 import org.apache.commons.dbcp.BasicDataSource;
 import ru.taskurotta.backend.ora.domain.SimpleTask;
 import ru.taskurotta.backend.ora.queue.OraQueueDao;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * User: greg
@@ -23,15 +22,11 @@ public class CreationTask implements Runnable {
     public void run() {
         OraQueueDao dbDAO = new OraQueueDao(dataSource);
         int count = 0;
-        try {
-            System.out.println("Starting adding data");
-            while (count < 100000) {
-                count++;
-                dbDAO.enqueueTask(new SimpleTask(UUID.randomUUID(), new Date(), 0, "DEF"), "QUEUE_TEST");
-            }
-            System.out.println("Stopped adding data");
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        System.out.println("Starting adding data");
+        while (count < 100000) {
+            count++;
+            dbDAO.enqueueTask(new SimpleTask(UUID.randomUUID(), new Date(), 0, "DEF"), "QUEUE_TEST");
         }
+        System.out.println("Stopped adding data");
     }
 }
