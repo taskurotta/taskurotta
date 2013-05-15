13ee370cc538963d6f66a76b9fa2121c028d6278
stukushin <stukushin@gmail.com>
2013-05-15 16:37:22 +0700

    Add jmx management.

---
+++ wf-bootstrap/src/main/java/ru/taskurotta/bootstrap/BootstrapMBean.java
@@ -0,0 +1,10 @@
+package ru.taskurotta.bootstrap;
+
+/**
+ * User: stukushin
+ * Date: 15.05.13
+ * Time: 15:57
+ */
+public interface BootstrapMBean {
+    void stop();
+}
--- wf-bootstrap/src/main/java/ru/taskurotta/bootstrap/Main.java
+++ wf-bootstrap/src/main/java/ru/taskurotta/bootstrap/Main.java
@@ -2,16 +2,33 @@ package ru.taskurotta.bootstrap;
 
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 /**
- * User: romario
+ * User: romario, stukushin
  * Date: 2/12/13
  * Time: 5:38 PM
  */
 public class Main {
 
     public static void main(String[] args) throws IOException, ArgumentParserException, ClassNotFoundException {
-        new Bootstrap(args).start();
+        Bootstrap bootstrap = new Bootstrap(args);
+        bootstrap.start();
+
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        try {
+            ObjectName name = new ObjectName("ru.taskurotta.bootstrap.jmx:type=BootstrapMBean");
+            mBeanServer.registerMBean(bootstrap, name);
+        } catch (MalformedObjectNameException | NotCompliantMBeanException | InstanceAlreadyExistsException | MBeanRegistrationException e) {
+            throw new RuntimeException("Catch exception while start new instance", e);
+        }
+
     }
 }
