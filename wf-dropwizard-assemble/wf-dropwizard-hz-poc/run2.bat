d653d617a4d86be46dbdea172e71c21979181d06
dudin <Dmitry.Dudin@nsk.fccland.ru>
2013-07-03 17:58:16 +0700

    post release pom.xml version updates

--- wf-dropwizard-assemble/wf-dropwizard-hz-poc/run2.bat
+++ wf-dropwizard-assemble/wf-dropwizard-hz-poc/run2.bat
@@ -1,1 +1,1 @@
-start java -Dts.hz.port=7777 -Ddw.http.port=8821 -Ddw.http.adminPort=8822 -Ddw.logging.file.currentLogFilename="./target/logs/service2.log" -jar target/wf-dropwizard-hz-poc-0.1.0-SNAPSHOT.jar server src/main/resources/conf.yml
+start java -Dts.hz.port=7777 -Ddw.http.port=8821 -Ddw.http.adminPort=8822 -Ddw.logging.file.currentLogFilename="./target/logs/service2.log" -jar target/wf-dropwizard-hz-poc-0.2.0-SNAPSHOT.jar server src/main/resources/conf.yml
