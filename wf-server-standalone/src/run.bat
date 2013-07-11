374a83ba0b20e5246a828038d15ba703f7b662fb
stukushin <stukushin@gmail.com>
2013-07-11 16:09:46 +0700

    Remove standalone server.

--- wf-server-standalone/src/run.bat
+++
@@ -1,1 +0,0 @@
-java -Dcom.sun.management.jmxremote.port=9999  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar ../target/wf-server-standalone-0.2.0-SNAPSHOT.jar
