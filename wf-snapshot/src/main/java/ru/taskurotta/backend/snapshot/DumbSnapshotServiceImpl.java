d03cca9b986c49eff67938ae20cd9bae17dd4caa
Grigorii Tkachuk <tkgreg@me.com>
2013-07-12 14:27:46 +0900

    Dumb snapshot service added

---
+++ wf-snapshot/src/main/java/ru/taskurotta/backend/snapshot/DumbSnapshotServiceImpl.java
@@ -0,0 +1,35 @@
+package ru.taskurotta.backend.snapshot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * User: greg
+ */
+public class DumbSnapshotServiceImpl implements SnapshotService {
+    private final static Logger logger = LoggerFactory.getLogger(DumbSnapshotServiceImpl.class);
+
+    @Override
+    public void createSnapshot(UUID processID) {
+        logger.debug("Dumb snapshot created");
+    }
+
+    @Override
+    public Snapshot getSnapshot(UUID snapshotId) {
+        return null;
+    }
+
+    @Override
+    public List<Snapshot> getSnapshotByProcessId(UUID snapshotId) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void saveSnapshot(Snapshot snapshot) {
+
+    }
+}
