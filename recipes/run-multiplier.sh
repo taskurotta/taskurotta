4be818137a8b5fc24819483c27e774666328571a
Grigorii Tkachuk <tkgreg@me.com>
2013-09-04 11:29:51 +0900

    project modules renamed

---
+++ recipes/run-multiplier.sh
@@ -0,0 +1,1 @@
+java -jar ./target/recipes-0.3.0-SNAPSHOT.jar -r ru/taskurotta/recipes/multiplier/wf-config-jersey.yml
--- wf-recipes/run-multiplier.sh
+++
@@ -1,1 +0,0 @@
-java -jar ./target/wf-recipes-0.3.0-SNAPSHOT.jar -r ru/taskurotta/recipes/multiplier/wf-config-jersey.yml
