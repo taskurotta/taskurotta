ef03b648720d00cbf037f4628fdb4f63e71ab0f7
stukushin <stukushin@gmail.com>
2013-10-01 17:37:12 +0700

    Add oracle perfomance test.

--- backend-ora/src/test/java/ru/taskurotta/backend/ora/DbConnect.java
+++ backend-ora/src/test/java/ru/taskurotta/backend/ora/DbConnect.java
@@ -17,7 +17,7 @@ public class DbConnect {
         dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
         dataSource.setUsername("taskurotta");
         dataSource.setPassword("taskurotta");
-        dataSource.setUrl("jdbc:oracle:thin:@//taskurotta-db:1521/orcl");
+        dataSource.setUrl("jdbc:oracle:thin:@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = db1.fccland.ru)(PORT = 1521))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = orcl.fccland.ru)))");
     }
 
     public BasicDataSource getDataSource() {
