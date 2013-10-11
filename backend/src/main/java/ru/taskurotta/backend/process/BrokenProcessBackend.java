4f89a5137384fab94f3f90c341455a6c5c17fe12
stukushin <stukushin@gmail.com>
2013-10-11 18:27:19 +0700

    Create broken process backend and VO.

---
+++ backend/src/main/java/ru/taskurotta/backend/process/BrokenProcessBackend.java
@@ -0,0 +1,9 @@
+package ru.taskurotta.backend.process;
+
+/**
+ * User: stukushin
+ * Date: 11.10.13
+ * Time: 18:24
+ */
+public interface BrokenProcessBackend {
+}
---
+++ backend/src/main/java/ru/taskurotta/backend/process/SearchObject.java
@@ -0,0 +1,9 @@
+package ru.taskurotta.backend.process;
+
+/**
+ * User: stukushin
+ * Date: 11.10.13
+ * Time: 18:25
+ */
+public class SearchObject {
+}
