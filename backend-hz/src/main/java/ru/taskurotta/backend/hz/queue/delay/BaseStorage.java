bbfb065d4ba369f83e30ae98b7f43d7321ed8ca7
stukushin <stukushin@gmail.com>
2013-12-04 14:19:29 +0700

    Revert "First implementation BaseStorage."
    This reverts commit 716b1a167943e068085f38e2b6034711ac1e6721.

--- backend-hz/src/main/java/ru/taskurotta/backend/hz/queue/delay/BaseStorage.java
+++ backend-hz/src/main/java/ru/taskurotta/backend/hz/queue/delay/BaseStorage.java
@@ -2,14 +2,8 @@ package ru.taskurotta.backend.hz.queue.delay;
 
 import com.hazelcast.core.IMap;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * User: romario
@@ -18,9 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class BaseStorage implements Storage {
 
-    private IMap<Long, Set<Object>> storage;
-
-    private final ReentrantLock lock = new ReentrantLock();
+    private IMap storage;
 
     public BaseStorage(IMap storage) {
         this.storage = storage;
@@ -28,56 +20,11 @@ public class BaseStorage implements Storage {
 
     @Override
     public boolean add(Object o, int delayTime, TimeUnit unit) {
-
-        long delayMilliseconds = TimeUnit.MILLISECONDS.convert(delayTime, unit);
-
-        Set<Object> objects = storage.get(delayMilliseconds);
-
-        if (objects == null) {
-            final ReentrantLock lock = this.lock;
-
-            try {
-                lock.lock();
-
-                objects = storage.get(delayMilliseconds);
-                if (objects == null) {
-                    objects = new HashSet<>();
-                    storage.put(delayMilliseconds, objects);
-                }
-            } finally {
-                lock.unlock();
-            }
-        }
-
-        return objects.add(o);
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public Collection getReadyItems() {
-
-        Collection<Object> collection = new ArrayList<>();
-
-        long now = System.currentTimeMillis();
-
-        SortedSet<Long> keys = new TreeSet<>(storage.keySet());
-
-        final ReentrantLock lock = this.lock;
-
-        for (Long key : keys) {
-
-            if (key > now) {
-                break;
-            }
-
-            try {
-                lock.lock();
-
-                collection.addAll(storage.remove(key));
-            } finally {
-                lock.unlock();
-            }
-        }
-
-        return collection;
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
