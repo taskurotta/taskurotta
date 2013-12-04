86f9404888a313b67254d7c457d05bb5865765ff
Grigorii Tkachuk <tkgreg@me.com>
2013-12-04 11:54:24 +0700

    recipes tests improvement

--- recipes/src/test/java/ru/taskurotta/recipes/RecipesRunner.java
+++ recipes/src/test/java/ru/taskurotta/recipes/RecipesRunner.java
@@ -10,11 +10,18 @@ import java.io.IOException;
  */
 public final class RecipesRunner {
 
+    public static final String ENVIRONMENT_CONF = "recipesEnv";
+    public static final String REAL_ENV = "REAL";
+
     public static void run(String packageName) throws ArgumentParserException, IOException, ClassNotFoundException {
         new Bootstrap(packageName + getProfile()).start();
     }
 
     private static String getProfile() {
+        final String str = System.getenv(ENVIRONMENT_CONF);
+        if (str != null && str.equals(REAL_ENV)) {
+            return "conf-jersey.yml";
+        }
         return "conf.yml";
     }
 }
--- recipes/src/test/java/ru/taskurotta/recipes/RecursionsTest.java
+++ recipes/src/test/java/ru/taskurotta/recipes/RecursionsTest.java
@@ -9,6 +9,7 @@ import ru.taskurotta.test.flow.FlowArbiterFactory;
 import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
+import static ru.taskurotta.recipes.RecipesRunner.run;
 
 /**
  * Created by void 04.04.13 20:18
@@ -18,8 +19,7 @@ public class RecursionsTest {
 	//@org.junit.Ignore
 	@Test
 	public void testRecursion() throws ArgumentParserException, IOException, ClassNotFoundException {
-		new Bootstrap("ru/taskurotta/recipes/recursion/wf-config.yml").start();
-
+        run("ru/taskurotta/recipes/recursion/");
 		BasicFlowArbiter arbiter = (BasicFlowArbiter) new FlowArbiterFactory().getInstance(); // created in spring context
 		assertTrue(arbiter.waitForFinish(10000));
 	}
