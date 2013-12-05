f3cb16ab0e9967dfe3e7731791b9b88d6718ffc8
stukushin <stukushin@gmail.com>
2013-12-05 15:14:14 +0700

    Add mock implementation for GarbageCollectorBackend.

--- backend/src/main/java/ru/taskurotta/backend/gc/MockGarbageCollectorBackend.java
+++ backend/src/main/java/ru/taskurotta/backend/gc/MockGarbageCollectorBackend.java
@@ -1,9 +1,23 @@
 package ru.taskurotta.backend.gc;
 
+import ru.taskurotta.backend.config.ConfigBackend;
+import ru.taskurotta.backend.dependency.links.GraphDao;
+import ru.taskurotta.backend.storage.ProcessBackend;
+import ru.taskurotta.backend.storage.TaskDao;
+
+import java.util.UUID;
+
 /**
  * User: stukushin
  * Date: 05.12.13
  * Time: 15:11
  */
 public class MockGarbageCollectorBackend implements GarbageCollectorBackend {
+
+    public MockGarbageCollectorBackend(ConfigBackend configBackend, ProcessBackend processBackend, GraphDao graphDao, TaskDao taskDao) {}
+
+    @Override
+    public void delete(UUID processId, String actorId) {
+        // do nothing
+    }
 }
