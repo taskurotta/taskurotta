788360e8058bedc9168529a6ed334c9899ecb1f1
stukushin <stukushin@gmail.com>
2013-12-05 15:14:02 +0700

    Add mock implementation for GarbageCollectorBackend.

---
+++ backend/src/main/java/ru/taskurotta/backend/gc/MockGarbageCollectorBackend.java
@@ -0,0 +1,9 @@
+package ru.taskurotta.backend.gc;
+
+/**
+ * User: stukushin
+ * Date: 05.12.13
+ * Time: 15:11
+ */
+public class MockGarbageCollectorBackend implements GarbageCollectorBackend {
+}
