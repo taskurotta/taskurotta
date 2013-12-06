ec497d2e5492ed5a250952fd731cbdddfa1388b5
stukushin <stukushin@gmail.com>
2013-12-06 17:50:23 +0700

    Fix bug in ObjectTypes.

--- backend-hz/src/main/java/ru/taskurotta/backend/hz/serialization/ObjectTypes.java
+++ backend-hz/src/main/java/ru/taskurotta/backend/hz/serialization/ObjectTypes.java
@@ -10,7 +10,7 @@ public class ObjectTypes {
     public static final int ACTOR_SCHEDULING_OPTIONS_CONTAINER = 4;
     public static final int ARG_CONTAINER = 3;
     public static final int BASE_STORAGE_ITEM = 13;
-    public static final int COMMON_STORAGE_ITEM = 13;
+    public static final int COMMON_STORAGE_ITEM = 14;
     public static final int DECISION_CONTAINER = 8;
     public static final int DECISION_ROW_STREAM = 2;
     public static final int ERROR_CONTAINER = 7;
