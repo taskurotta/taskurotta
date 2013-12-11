c32087f1e0f996dd93ca185570ddced889c48cd0
stukushin <stukushin@gmail.com>
2013-12-11 20:22:45 +0700

    Add hzQueueConfigSupport.

--- hazelcast/src/main/java/ru/taskurotta/hazelcast/delay/BaseQueueFactory.java
+++ hazelcast/src/main/java/ru/taskurotta/hazelcast/delay/BaseQueueFactory.java
@@ -2,6 +2,10 @@ package ru.taskurotta.hazelcast.delay;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IQueue;
+import ru.taskurotta.hazelcast.HzQueueConfigSupport;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * User: romario
@@ -12,18 +16,53 @@ public class BaseQueueFactory implements QueueFactory {
 
     private HazelcastInstance hazelcastInstance;
     private StorageFactory storageFactory;
+    private HzQueueConfigSupport hzQueueConfigSupport;
+
+    private transient final ReentrantLock lock = new ReentrantLock();
+    private ConcurrentHashMap<String, DelayIQueue> queueMap = new ConcurrentHashMap<>();
 
     public BaseQueueFactory(HazelcastInstance hazelcastInstance, StorageFactory storageFactory) {
         this.hazelcastInstance = hazelcastInstance;
         this.storageFactory = storageFactory;
     }
 
+    public BaseQueueFactory(HazelcastInstance hazelcastInstance, StorageFactory storageFactory, HzQueueConfigSupport hzQueueConfigSupport) {
+        this.hazelcastInstance = hazelcastInstance;
+        this.storageFactory = storageFactory;
+        this.hzQueueConfigSupport = hzQueueConfigSupport;
+    }
+
     @Override
     public DelayIQueue create(String queueName) {
 
-        IQueue queue = hazelcastInstance.getQueue(queueName);
-        Storage storage = storageFactory.createStorage(queueName);
+        DelayIQueue delayIQueue = queueMap.get(queueName);
 
-        return new DelayIQueue(queue, storage);
+        if (delayIQueue == null) {
+
+            final ReentrantLock lock = this.lock;
+            lock.lock();
+
+            try {
+                delayIQueue = queueMap.get(queueName);
+                if (delayIQueue != null) {
+                    return delayIQueue;
+                }
+
+                if (hzQueueConfigSupport != null) {
+                    hzQueueConfigSupport.createQueueConfig(queueName);
+                }
+
+                IQueue iQueue = hazelcastInstance.getQueue(queueName);
+                Storage storage = storageFactory.createStorage(queueName);
+
+                delayIQueue = new DelayIQueue(iQueue, storage);
+
+                queueMap.put(queueName, delayIQueue);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        return delayIQueue;
     }
 }
