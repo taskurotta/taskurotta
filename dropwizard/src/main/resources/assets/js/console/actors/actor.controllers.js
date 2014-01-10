7d20949be5a934ffc0a702748552ffa46dbae47d
dudin <Dmitry.Dudin@nsk.fccland.ru>
2014-01-10 10:45:47 +0700

    Console actor view refactoring

--- dropwizard/src/main/resources/assets/js/console/actors/actor.controllers.js
+++ dropwizard/src/main/resources/assets/js/console/actors/actor.controllers.js
@@ -1,210 +1,151 @@
 angular.module("console.actor.controllers", ['console.services', 'ui.bootstrap.modal', 'ngRoute'])
 
-.controller("actorListController", ['$scope', '$$data', 'tskActors', '$log', '$modal', '$location', '$cookieStore', function ($scope, $$data, tskActors, $log, $modal, $location, $cookieStore) {
-    $scope.feedback = "";
-    $scope.initialized = false;
+    .controller("actorListController", ['$scope', '$$data', 'tskActors', '$log', '$modal', '$location', function ($scope, $$data, tskActors, $log, $modal, $location) {
+        $scope.initialized = false;
+        $scope.metrics = [];
+        $scope.metricsData = {};
+        $scope.feedback = {};
 
-    //Init paging object
-    $scope.actorsPage = {
-        pageSize: 10,
-        pageNumber: 1,
-        totalCount: 0,
-        items: []
-    };
+        //Init paging object
+        $scope.actorsPage = {
+            pageSize: 10,
+            pageNumber: 1,
+            totalCount: 0,
+            items: []
+        };
 
-    $scope.selection = {
-        actorIds: {}
-    };
+        $scope.periods = ["day", "hour"];
 
-    $scope.getSelectedActorIds = function() {
-        var result = [];
-        for (var key in $scope.selection.actorIds) {
-            if ($scope.selection.actorIds[key]) {
-                result.push(key);
-            }
-        }
-        return result;
-    };
+        $scope.selection = {
+            metric: {},
+            metricPeriod: {}
+        };
 
-    $scope.navigateToCompareView = function() {
-        $cookieStore.put('actors.compare.actorIds', $scope.getSelectedActorIds());
-        $location.url('/actors/compare/');
-    };
-
-    $scope.isSeveralActorsSelected = function(){
-        return $scope.getSelectedActorIds().length > 1;
-    };
-
-    $scope.isActorSelected = function(actorId) {
-        var selected = $scope.getSelectedActorIds();
-        for (var i = 0; i<selected.length; i++) {
-            if (selected[i] == actorId) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    $scope.isSelectionLinkActive = function(actorId) {
-        return $scope.isSeveralActorsSelected() && $scope.isActorSelected(actorId);
-    };
-
-    var setBlockedState = function(actorId, isBlocked) {
-        var modalInstance = $modal.open({
-            templateUrl: '/partials/view/modal/approve_msg.html',
-            windowClass: 'approve'
-        });
-
-        modalInstance.result.then(function(okMess) {
-            tskActors.setActorBlocked(actorId, isBlocked).then(function(success) {
-                $log.log("Actor ["+actorId+"] have been set to blocked["+isBlocked+"]");
-                $scope.update();
-            }, function(error) {
-                $log.error("Error setting blocked["+isBlocked+"] for actor ["+actorId+"]: " + angular.toJson(error));
-                $scope.feedback = error;
-                $scope.update();
+        var setBlockedState = function(actorId, isBlocked) {
+            var modalInstance = $modal.open({
+                templateUrl: '/partials/view/modal/approve_msg.html',
+                windowClass: 'approve'
             });
-        }, function(cancelMsg) {
-            //do nothing
-        });
-    };
 
-    $scope.blockActor = function(actorId) {
-        setBlockedState(actorId, true);
-    };
+            modalInstance.result.then(function(okMess) {
+                tskActors.setActorBlocked(actorId, isBlocked).then(function(success) {
+                    $log.log("Actor ["+actorId+"] have been set to blocked["+isBlocked+"]");
+                    $scope.update();
+                }, function(error) {
+                    $log.error("Error setting blocked["+isBlocked+"] for actor ["+actorId+"]: " + angular.toJson(error));
+                    $scope.feedback = error;
+                    $scope.update();
+                });
+            }, function(cancelMsg) {
+                //do nothing
+            });
+        };
 
-    $scope.unblockActor = function(actorId) {
-        setBlockedState(actorId, false);
-    };
+        $scope.blockActor = function(actorId) {
+            setBlockedState(actorId, true);
+        };
 
-    $scope.hasHourRateData = function(actorVO) {
-        var result = false;
-        if (actorVO.queueState) {
-            result = (actorVO.queueState.totalOutHour && actorVO.queueState.totalOutHour>=0)
-                || (actorVO.queueState.totalInHour && actorVO.queueState.totalInHour>=0);
-        }
-        return result;
-    };
+        $scope.unblockActor = function(actorId) {
+            setBlockedState(actorId, false);
+        };
 
-    $scope.hasDayRateData = function(actorVO) {
-        var result = false;
-        if (actorVO.queueState) {
-            result = (actorVO.queueState.totalOutDay && actorVO.queueState.totalOutDay>=0)
-                || (actorVO.queueState.totalInDay && actorVO.queueState.totalInDay>=0);
-        }
-        return result;
-    };
-
-    $scope.totalTasks = function () {
-        var result = 0;
-        if($scope.actorsPage.items) {
-            for (var i = 0; i < $scope.actorsPage.items.length; i++) {
-                result = result + $scope.actorsPage.items[i].count;
+        $scope.hasHourRateData = function(actorVO) {
+            var result = false;
+            if (actorVO.queueState) {
+                result = (actorVO.queueState.totalOutHour && actorVO.queueState.totalOutHour>=0)
+                    || (actorVO.queueState.totalInHour && actorVO.queueState.totalInHour>=0);
             }
-        }
-        return result;
-    };
+            return result;
+        };
 
-    //Updates queues states  by polling REST resource
-    $scope.update = function () {
-
-        tskActors.listActors($scope.actorsPage.pageNumber, $scope.actorsPage.pageSize).then(function (value) {
-            $scope.actorsPage = value.data || {};
-            $scope.initialized = true;
-            $log.info("actorListController: successfully updated actors list: " + angular.toJson($scope.actorsPage));
-            $cookieStore.put('actors.compare.actorIds', $scope.getSelectedActorIds());
-        }, function (errReason) {
-            $scope.feedback = errReason;
-            $scope.initialized = true;
-            $log.error("actorListController: actor list update failed: " + angular.toJson($scope.feedback));
-        });
-
-    };
-
-    $scope.initSelection = function() {
-        var selected = $cookieStore.get('actors.compare.actorIds');
-        if (selected && selected.length>0) {
-            for (var i = 0; i<selected.length; i++) {
-                $scope.selection.actorIds[selected[i]] = true;
+        $scope.hasDayRateData = function(actorVO) {
+            var result = false;
+            if (actorVO.queueState) {
+                result = (actorVO.queueState.totalOutDay && actorVO.queueState.totalOutDay>=0)
+                    || (actorVO.queueState.totalInDay && actorVO.queueState.totalInDay>=0);
             }
-        }
-    };
+            return result;
+        };
 
-    //Initialization:
-    $scope.initSelection();
-    $scope.update();
+        //Updates queues states  by polling REST resource
+        $scope.update = function () {
 
-}])
+            tskActors.listActors($scope.actorsPage.pageNumber, $scope.actorsPage.pageSize).then(function (value) {
+                $scope.actorsPage = value.data || {};
+                $log.info("actorListController: successfully updated actors list: " + angular.toJson($scope.actorsPage));
+                tskActors.listMetrics().then(function (success) {
+                    $scope.metrics = success.data;
+                    for (var i = 0; i<$scope.metrics.length; i++) {//period "hour" by default for all metrics (if other value has not been specified)
+                        if (!$scope.selection.metricPeriod[$scope.metrics[i]]) {
+                            $scope.selection.metricPeriod[$scope.metrics[i]] = "hour";
+                        }
+                    }
 
+                    $scope.updateMetricsData();
+                    $scope.initialized = true;
+                }, function (fail) {
+                    $scope.feedback = fail.data;
+                    $scope.initialized = true;
+                });
+            }, function (errReason) {
+                $scope.feedback = errReason;
+                $scope.initialized = true;
+                $log.error("actorListController: actor list update failed: " + angular.toJson($scope.feedback));
+            });
 
-.controller("actorCompareController", function ($scope, $location, $log, $cookieStore, tskActors) {
+        };
 
-    $scope.actorIds = $cookieStore.get('actors.compare.actorIds');
-
-    $scope.metrics = [];
-    $scope.metricsData = {};
-    $scope.feedback = {};
-    $scope.selection = {
-        metric: {
-            "enqueue": true,
-            "successfulPoll": true
-        }
-    };
-
-
-    $scope.getSelectedMetricNames = function() {
-        var result = [];
-        for(var key in $scope.selection.metric) {
-            if ($scope.selection.metric[key]) {
-                result.push(key);
+        $scope.getSelectedMetricNames = function() {
+            var result = [];
+            for(var key in $scope.selection.metric) {
+                if ($scope.selection.metric[key]) {
+                    result.push(key);
+                }
             }
-        }
-        return result;
-    }
+            return result;
+        };
 
+        $scope.updateMetricsData = function() {
+            var actorIds = [];
+            for (var i = 0; i< $scope.actorsPage.items.length; i++) {
+                actorIds.push($scope.actorsPage.items[i].actorId);
+            }
+            var metricsArr = $scope.getSelectedMetricNames();
+            //$log.log("metricsArr: " + angular.toJson(metricsArr) + ", actorIds: " + angular.toJson(actorIds));
 
-    $scope.updateMetricsData = function() {
-        //var selectedMetrics = $scope.getSelectedMetricNames();
-        if ($scope.actorIds.length>0 && $scope.metrics.length>0) {
-            tskActors.getMetricsData($scope.actorIds, $scope.metrics).then(function (success) {
+            tskActors.getMetricsData(actorIds, metricsArr).then(function (success) {
                 $scope.metricsData = success.data;
             }, function (error) {
                 $scope.feedback = error.data;
             });
-        }
-    };
+        };
 
-    $scope.getMetricsRows = function(columns) {
-        var result = [];//array of row start indexes
-        for (var i = 0; i<$scope.metrics.length; i = i+columns) {
-            result.push(i);
-        }
-        return result;
-    };
-
-    $scope.update = function () {
-        tskActors.listMetrics().then(function (success) {
-            $scope.metrics = success.data;
-            $scope.updateMetricsData();
-        }, function (fail) {
-            $scope.feedback = fail.data;
-        });
-    };
-
-
-    $scope.getActorStatData = function(metric, actor, collection) {
-        for (var i = 0; i < collection.length; i++) {
-            var item = collection[i];
-            if (item.metricName==metric && item.datasetName==actor ) {
-                return item;
+        $scope.getMetricsRows = function(columns) {
+            var result = [];//array of row start indexes
+            for (var i = 0; i<$scope.metrics.length; i = i+columns) {
+                result.push(i);
             }
-        }
-        return {};
-    };
+            return result;
+        };
 
-    $scope.update();
+        $scope.getActorStatData = function(metric, actor, collection) {
+            var result = {};
+            for (var i = 0; i < collection.length; i++) {
+                var item = collection[i];
+                if (item.metricName==metric && item.datasetName==actor ) {
+                    result = item;
+                    break;
+                }
+            }
+            //$log.log("getActorStatData with args: metric["+metric+"], actor["+actor+"], collection["+angular.toJson(collection)+"] result is ["+angular.toJson(result)+"]");
+            return result;
+        };
 
-});
+        //Initialization:
+        $scope.update();
+        $scope.$watch("getSelectedMetricNames().length", function(newVal, oldVal) {
+            $scope.updateMetricsData();
+        });
 
-
-
+    }])
+;
