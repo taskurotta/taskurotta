13a792bbef3fcfabbd8da5f85d7c653f646909e8
stukushin <stukushin@gmail.com>
2014-01-17 14:28:47 +0700

    Add tests for queue memory-limit.

---
+++ hazelcast/src/test/java/ru/taskurotta/hz/test/queue/FakeQueueStore.java
@@ -0,0 +1,9 @@
+package ru.taskurotta.hz.test.queue;
+
+/**
+ * User: stukushin
+ * Date: 17.01.14
+ * Time: 12:40
+ */
+public class FakeQueueStore {
+}
---
+++ hazelcast/src/test/java/ru/taskurotta/hz/test/queue/QueueTest.java
@@ -0,0 +1,16 @@
+package ru.taskurotta.hz.test.queue;
+
+import org.junit.Test;
+
+/**
+ * User: stukushin
+ * Date: 16.01.14
+ * Time: 18:31
+ */
+public class QueueTest {
+
+    @Test
+    public void storeTest() {
+
+    }
+}
