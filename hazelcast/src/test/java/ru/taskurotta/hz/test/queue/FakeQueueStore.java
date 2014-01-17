c5e56595247b397d766bfaf9b29d635ae434ab94
stukushin <stukushin@gmail.com>
2014-01-17 14:29:03 +0700

    Add tests for queue memory-limit.

--- hazelcast/src/test/java/ru/taskurotta/hz/test/queue/FakeQueueStore.java
+++ hazelcast/src/test/java/ru/taskurotta/hz/test/queue/FakeQueueStore.java
@@ -1,9 +1,49 @@
 package ru.taskurotta.hz.test.queue;
 
+import com.hazelcast.core.QueueStore;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * User: stukushin
  * Date: 17.01.14
  * Time: 12:40
  */
-public class FakeQueueStore {
+public class FakeQueueStore implements QueueStore<Integer> {
+    @Override
+    public void store(Long key, Integer value) {
+
+    }
+
+    @Override
+    public void storeAll(Map<Long, Integer> map) {
+
+    }
+
+    @Override
+    public void delete(Long key) {
+
+    }
+
+    @Override
+    public void deleteAll(Collection<Long> keys) {
+
+    }
+
+    @Override
+    public Integer load(Long key) {
+        return null;
+    }
+
+    @Override
+    public Map<Long, Integer> loadAll(Collection<Long> keys) {
+        return null;
+    }
+
+    @Override
+    public Set<Long> loadAllKeys() {
+        return null;
+    }
 }
--- hazelcast/src/test/java/ru/taskurotta/hz/test/queue/QueueTest.java
+++ hazelcast/src/test/java/ru/taskurotta/hz/test/queue/QueueTest.java
@@ -1,6 +1,12 @@
 package ru.taskurotta.hz.test.queue;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IQueue;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.taskurotta.hazelcast.HzQueueConfigSupport;
 
 /**
  * User: stukushin
@@ -9,8 +15,68 @@ import org.junit.Test;
  */
 public class QueueTest {
 
-    @Test
-    public void storeTest() {
+    private final String queueName = "testQueue";
 
+    @Ignore
+    @Test
+    public void fakeStoreTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/ru.taskurotta.hz.test.queue/hz-queue-fake.xml");
+        HazelcastInstance hazelcastInstance = applicationContext.getBean("hzInstance", HazelcastInstance.class);
+
+        IQueue<Integer> iQueue = hazelcastInstance.getQueue(queueName);
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            iQueue.add(i);
+
+            if (i % 10000 == 0) {
+                System.out.println("Added [" + i + "] elements");
+            }
+        }
+    }
+
+    @Ignore
+    @Test
+    public void memoryStoreTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/ru.taskurotta.hz.test.queue/hz-queue-memory.xml");
+        HazelcastInstance hazelcastInstance = applicationContext.getBean("hzInstance", HazelcastInstance.class);
+        MemoryQueueStore queueStore = applicationContext.getBean("queueStore", MemoryQueueStore.class);
+
+        IQueue<Integer> iQueue = hazelcastInstance.getQueue(queueName);
+
+        for (int i = 0; i < 20; i++) {
+            iQueue.add(i);
+        }
+
+        int memoryLimit= Integer.parseInt(hazelcastInstance.getConfig().getQueueConfig(queueName).getQueueStoreConfig().getProperty("memory-limit"));
+
+        System.out.println("Queue size: [" + iQueue.size() + "], store size: [" + queueStore.size() + "], memory-limit: [" + memoryLimit + "]");
+
+        queueStore.clear();
+
+        System.out.println("After clear: queue size: [" + iQueue.size() + "], store size: [" + queueStore.size() + "]");
+
+        while (!iQueue.isEmpty()) {
+            System.out.println(iQueue.poll());
+        }
+    }
+
+    @Ignore
+    @Test
+    public void mongoStoreTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/ru.taskurotta.hz.test.queue/hz-queue-mongo.xml");
+        HazelcastInstance hazelcastInstance = applicationContext.getBean("hzInstance", HazelcastInstance.class);
+        HzQueueConfigSupport hzQueueConfigSupport = applicationContext.getBean("queueConfigSupport", HzQueueConfigSupport.class);
+
+        hzQueueConfigSupport.createQueueConfig(queueName);
+
+        IQueue<Integer> iQueue = hazelcastInstance.getQueue(queueName);
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            iQueue.add(i);
+
+            if (i % 100000 == 0) {
+                System.out.println("Added [" + i + "] elements");
+            }
+        }
     }
 }
