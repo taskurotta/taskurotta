1497fee33fbcb0cf8d392ce319c2437d29755de9
stukushin <stukushin@gmail.com>
2014-10-16 15:53:52 +0700

    Add test for long time working.

--- test/src/main/java/ru/taskurotta/test/time/LifeTimeProfiler.java
+++ test/src/main/java/ru/taskurotta/test/time/LifeTimeProfiler.java
@@ -1,10 +1,84 @@
 package ru.taskurotta.test.time;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.taskurotta.RuntimeProcessor;
+import ru.taskurotta.bootstrap.profiler.Profiler;
+import ru.taskurotta.client.TaskSpreader;
+import ru.taskurotta.core.Task;
+import ru.taskurotta.core.TaskDecision;
+
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * User: stukushin
  * Date: 16.10.2014
  * Time: 14:36
  */
 
-public class LifeTimeProfiler {
+public class LifeTimeProfiler implements Profiler {
+
+    private static final Logger logger = LoggerFactory.getLogger(LifeTimeProfiler.class);
+
+    private AtomicLong startTime = new AtomicLong(System.currentTimeMillis());
+    private AtomicLong pollCounter = new AtomicLong();
+    private ReentrantLock lock = new ReentrantLock();
+
+    public LifeTimeProfiler(Class actorClass, Properties properties) {}
+
+    @Override
+    public RuntimeProcessor decorate(final RuntimeProcessor runtimeProcessor) {
+        return new RuntimeProcessor() {
+            @Override
+            public TaskDecision execute(Task task) {
+                return runtimeProcessor.execute(task);
+            }
+
+            @Override
+            public Task[] execute(UUID processId, Runnable runnable) {
+                return runtimeProcessor.execute(processId, runnable);
+            }
+        };
+    }
+
+    @Override
+    public TaskSpreader decorate(final TaskSpreader taskSpreader) {
+        return new TaskSpreader() {
+            @Override
+            public Task poll() {
+                if (pollCounter.incrementAndGet() % 1000 == 0) {
+                    if (lock.tryLock()) {
+                        writeToLog(pollCounter.longValue());
+                        lock.unlock();
+                    }
+                }
+
+                return taskSpreader.poll();
+            }
+
+            @Override
+            public void release(TaskDecision taskDecision) {
+                taskSpreader.release(taskDecision);
+            }
+        };
+    }
+
+    @Override
+    public void cycleStart() {}
+
+    @Override
+    public void cycleFinish() {}
+
+    private void writeToLog(long poolCounter) {
+        long workDuration = (System.currentTimeMillis() - startTime.longValue()) / 1000;
+        if (workDuration == 0) {
+            return;
+        }
+
+        double pollAvgSpeed = poolCounter / workDuration;
+        logger.info("After [{}] tasks and [{}] seconds: pollAvgSpeed = [{}]/seconds", poolCounter, workDuration, pollAvgSpeed);
+    }
 }
--- test/src/main/java/ru/taskurotta/test/time/TaskCreator.java
+++ test/src/main/java/ru/taskurotta/test/time/TaskCreator.java
@@ -1,117 +1,65 @@
 package ru.taskurotta.test.time;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.beans.factory.annotation.Required;
 import ru.taskurotta.client.ClientServiceManager;
 import ru.taskurotta.client.DeciderClientProvider;
 import ru.taskurotta.test.fullfeature.decider.FullFeatureDeciderClient;
-import ru.taskurotta.test.stress.LifetimeProfiler;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static ru.taskurotta.test.stress.LifetimeProfiler.lastTime;
-import static ru.taskurotta.test.stress.LifetimeProfiler.stabilizationCounter;
-import static ru.taskurotta.test.stress.LifetimeProfiler.startTime;
-import static ru.taskurotta.test.stress.LifetimeProfiler.stopDecorating;
-import static ru.taskurotta.test.stress.LifetimeProfiler.taskCount;
-import static ru.taskurotta.test.stress.LifetimeProfiler.tasksForStat;
+public class TaskCreator {
 
-/**
- * User: greg
- */
-public class TaskCreator implements Runnable, ApplicationListener<ContextRefreshedEvent> {
+    private ExecutorService executorService;
+    private FullFeatureDeciderClient decider;
 
-    private static final Logger log = LoggerFactory.getLogger(TaskCreator.class);
-    private static final int THREADS_COUNT = 50;
+    private int threads;
+    private long duration;
+    private long timeout;
 
-    private boolean needRun = true;
-    public static CountDownLatch cd;
+    private long startTime = System.currentTimeMillis();
 
-    public static ExecutorService executorService;
-    public static FullFeatureDeciderClient deciderClient;
+    public void startTask() {
+        for (int i = 0; i < threads; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (startTime + duration >= System.currentTimeMillis()) {
+                        decider.start();
 
-    public TaskCreator(ClientServiceManager clientServiceManager, boolean needRun, int shotSize) {
-        this.needRun = needRun;
-
-        this.cd = new CountDownLatch(shotSize);
-
-        DeciderClientProvider clientProvider = clientServiceManager.getDeciderClientProvider();
-        deciderClient = clientProvider.getDeciderClient(FullFeatureDeciderClient.class);
-
-        executorService = Executors.newFixedThreadPool(THREADS_COUNT);
-        sendInitialTasks(shotSize);
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info("onApplicationEvent");
-        if (needRun) {
-            Executors.newSingleThreadExecutor().submit(this);
-        }
-    }
-
-
-    public void sendInitialTasks(int shotSize) {
-        log.info("Sending new " + shotSize + " tasks");
-
-        for (int i = 0; i < shotSize; i++) {
-            sendOneTask();
-        }
-
-    }
-
-    public static void sendOneTask() {
-
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                boolean done = false;
-                while (!done) {
-                    try {
-                        deciderClient.start();
-                        cd.countDown();
-                        done = true;
-                    } catch (Exception e) {
-                        log.warn("Start task rejected", e);
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(timeout);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
+
+                    System.exit(0);
                 }
-            }
-        });
-
-    }
-
-
-    @Override
-    public void run() {
-
-        while (stabilizationCounter.get() < 10) {
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            });
         }
-
-        long deltaTime = lastTime.get() - startTime.get();
-        double time = 1.0 * deltaTime / 1000.0;
-        long meanTaskCount = taskCount.get();
-        double rate = 1000.0 * meanTaskCount / deltaTime;
-        double totalDelta = LifetimeProfiler.totalDelta / (meanTaskCount / tasksForStat);
-
-        log.info("Total task count: " + taskCount);
-        log.info("Delta time: " + deltaTime);
-        log.info(String.format("TOTAL: tasks: %6d; time: %6.3f s; rate: %8.3f tps; totalDelta: %8.3f \n", meanTaskCount, time, rate, totalDelta));
-        stopDecorating.set(true);
-        log.info("Decoration stopped");
-        log.info("End");
-
-        System.exit(0);
     }
 
+    @Required
+    public void setClientServiceManager(ClientServiceManager clientServiceManager) {
+        DeciderClientProvider deciderClientProvider = clientServiceManager.getDeciderClientProvider();
+        decider = deciderClientProvider.getDeciderClient(FullFeatureDeciderClient.class);
+    }
+
+    @Required
+    public void setThreads(int threads) {
+        this.threads = threads;
+        this.executorService = Executors.newFixedThreadPool(threads);
+    }
+
+    @Required
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    @Required
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
 }
