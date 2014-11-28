35c5e0925a1eb493acc9aab59ab5ac5a5be5377d
stukushin <stukushin@gmail.com>
2014-11-28 12:57:33 +0600

    Fix process table script.

--- services-ora/src/main/resources/create_hz_ora.sql
+++ services-ora/src/main/resources/create_hz_ora.sql
@@ -1,12 +1,13 @@
 CREATE TABLE process (
-  process_id    VARCHAR2(36)  NOT NULL,
-  start_task_id VARCHAR2(36)  NOT NULL,
-  custom_id     VARCHAR2(256) NULL,
-  start_time    NUMBER        NOT NULL,
-  end_time      NUMBER        NULL,
-  state         NUMBER        NOT NULL,
-  return_value  CLOB          NULL,
-  START_JSON    CLOB          NULL,
+  process_id    VARCHAR2(36)        NOT NULL,
+  start_task_id VARCHAR2(36)        NOT NULL,
+  custom_id     VARCHAR2(256)       NULL,
+  start_time    NUMBER              NOT NULL,
+  end_time      NUMBER              NULL,
+  state         NUMBER              NOT NULL,
+  return_value  CLOB                NULL,
+  START_JSON    CLOB                NULL,
+  actor_id      VARCHAR2(500 char)  NOT NULL,
   PRIMARY KEY (process_id)
 )
 NOCOMPRESS
