d146eda9ddededbbddd228f5f205aba35f190972
dudin <dimadin@gmail.com>
2015-01-23 17:59:25 +0600

    Server YAML configs fixed to match DW 0.7.1

--- assemble/run-hz.bat
+++ assemble/run-hz.bat
@@ -1,1 +1,1 @@
-start java -Xmx128m -Xms128m -DassetsMode=dev -jar target/assemble-0.8.0-SNAPSHOT.jar server src/main/resources/hz.yml
+start java -Xmx128m -Xms128m -jar target/assemble-0.8.0-SNAPSHOT.jar server src/main/resources/hz.yml
