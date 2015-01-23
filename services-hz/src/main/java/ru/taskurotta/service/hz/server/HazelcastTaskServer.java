9c1db4235556322b90af8160c917abb58fa0e15f
stukushin <stukushin@gmail.com>
2015-01-23 14:16:53 +0600

    Add ProcessDecisionUnitOfWorkStreamSerializer.

--- services-hz/src/main/java/ru/taskurotta/service/hz/server/HazelcastTaskServer.java
+++ services-hz/src/main/java/ru/taskurotta/service/hz/server/HazelcastTaskServer.java
@@ -163,6 +163,28 @@ public class HazelcastTaskServer extends GeneralTaskServer {
         public Object getPartitionKey() {
             return taskDecision.getProcessId();
         }
+
+        public DecisionContainer getTaskDecision() {
+            return taskDecision;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ProcessDecisionUnitOfWork that = (ProcessDecisionUnitOfWork) o;
+
+            if (taskDecision != null ? !taskDecision.equals(that.taskDecision) : that.taskDecision != null)
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return taskDecision != null ? taskDecision.hashCode() : 0;
+        }
     }
 
 }
