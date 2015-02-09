5eaee0d514e6fd00cdbceda68f6a6541b0c9db82
romario13 <romario13@gmail.com>
2015-02-09 12:25:39 +0300

    Add: legacy style serialization test

--- services-hz/src/test/java/ru/taskurotta/hz/test/SerializationCompareTest.java
+++ services-hz/src/test/java/ru/taskurotta/hz/test/SerializationCompareTest.java
@@ -4,7 +4,16 @@ import com.hazelcast.nio.BufferObjectDataInput;
 import com.hazelcast.nio.BufferObjectDataOutput;
 import com.hazelcast.nio.serialization.DefaultSerializationServiceBuilder;
 import com.hazelcast.nio.serialization.SerializationService;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBDecoder;
+import com.mongodb.DBEncoder;
+import com.mongodb.DBObject;
+import com.mongodb.DefaultDBDecoder;
+import com.mongodb.DefaultDBEncoder;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.WriteConcern;
 import org.bson.BSONObject;
 import org.bson.io.BasicOutputBuffer;
 import org.bson.io.OutputBuffer;
@@ -12,6 +21,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import ru.taskurotta.hz.test.bson.BsonSerializationTest;
 import ru.taskurotta.mongodb.driver.BSerializationService;
 import ru.taskurotta.mongodb.driver.DBObjectCheat;
@@ -23,6 +34,7 @@ import ru.taskurotta.service.hz.serialization.TaskContainerStreamSerializer;
 import ru.taskurotta.service.hz.serialization.bson.TaskContainerBSerializer;
 import ru.taskurotta.transport.model.TaskContainer;
 
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 @Ignore
@@ -31,7 +43,7 @@ public class SerializationCompareTest {
     private final static Logger log = LoggerFactory.getLogger(SerializationCompareTest.class);
     public static final int COUNT = 100000;
 
-    private static int RUN_TESTS = Integer.parseInt("1111", 2);
+    private static int RUN_TESTS = Integer.parseInt("111111", 2);
 
     @Test
     public void test() throws Exception {
@@ -84,9 +96,7 @@ public class SerializationCompareTest {
         final TaskContainerBSerializer serializer = new TaskContainerBSerializer();
         StreamBSerializer[] array = new StreamBSerializer[]{serializer};
         BSerializationService bSerializationService = new BSerializationServiceImpl(array, null);
-
-        TestBEncoder testBEncoder = new TestBEncoder(bSerializationService);
-        BDecoder decoder = new BDecoder(serializer);
+        TestBEncoder encoder = new TestBEncoder(bSerializationService);
 
         DBObjectCheat<TaskContainer> dbObjectCheat = new DBObjectCheat<>(taskContainer);
 
@@ -99,11 +109,11 @@ public class SerializationCompareTest {
             start = System.currentTimeMillis();
             for (int i = 0; i < COUNT; i++) {
                 BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
-                testBEncoder.serialize(basicOutputBuffer, dbObjectCheat);
+                encoder.serialize(basicOutputBuffer, dbObjectCheat);
                 if (byteArray == null) {
                     byteArray = basicOutputBuffer.toByteArray();
                 }
-                testBEncoder.done();
+                encoder.done();
             }
 
             log.info("BSON: Serialization took {} ms, object size {} bytes", System.currentTimeMillis() - start, byteArray
@@ -114,8 +124,10 @@ public class SerializationCompareTest {
         // bson deserialization
         if ((RUN_TESTS & (1 << 3)) != 0) {
 
+            BDecoder decoder = new BDecoder(serializer);
+
             BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
-            testBEncoder.serialize(basicOutputBuffer, dbObjectCheat);
+            encoder.serialize(basicOutputBuffer, dbObjectCheat);
             byteArray = basicOutputBuffer.toByteArray();
             byteArray = Arrays.copyOfRange(byteArray, 4, byteArray.length);
 
@@ -125,6 +137,59 @@ public class SerializationCompareTest {
             }
 
             log.info("BSON: Deserialization took {} ms object size {} bytes", System.currentTimeMillis() - start,
+                    byteArray.length);
+        }
+
+
+        // DBObject serialization
+        if ((RUN_TESTS & (1 << 4)) != 0) {
+
+            MongoTemplate mongoTemplate = getMongoTemplate();
+            MongoConverter converter = mongoTemplate.getConverter();
+
+            DBEncoder dbEncoder = DefaultDBEncoder.FACTORY.create();
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+
+                DBObject dbObject = new BasicDBObject();
+                converter.write(taskContainer, dbObject);
+
+                BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
+
+                dbEncoder.writeObject(basicOutputBuffer, dbObject);
+
+                if (byteArray == null) {
+                    byteArray = basicOutputBuffer.toByteArray();
+                }
+            }
+
+            log.info("DBObject: Serialization took {} ms, object size {} bytes", System.currentTimeMillis() - start, byteArray
+                    .length);
+        }
+
+
+        // DBObject deserialization
+        if ((RUN_TESTS & (1 << 5)) != 0) {
+
+            MongoTemplate mongoTemplate = getMongoTemplate();
+            MongoConverter converter = mongoTemplate.getConverter();
+            DBEncoder dbEncoder = DefaultDBEncoder.FACTORY.create();
+            DBObject dbObject = new BasicDBObject();
+            converter.write(taskContainer, dbObject);
+            BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
+            dbEncoder.writeObject(basicOutputBuffer, dbObject);
+            byteArray = basicOutputBuffer.toByteArray();
+
+            DBDecoder dbDecoder = DefaultDBDecoder.FACTORY.create();
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+                DBObject decodedDbObject = dbDecoder.decode(byteArray, (DBCollection) null);
+                converter.read(TaskContainer.class, decodedDbObject);
+            }
+
+            log.info("DBObject: Deserialization took {} ms object size {} bytes", System.currentTimeMillis() - start,
                     byteArray.length);
         }
 
@@ -143,4 +208,17 @@ public class SerializationCompareTest {
         }
 
     }
+
+    private MongoTemplate getMongoTemplate() throws UnknownHostException {
+        ServerAddress serverAddress = new ServerAddress("127.0.0.1", 27017);
+        MongoClient mongoClient = new MongoClient(serverAddress);
+
+        WriteConcern writeConcern = new WriteConcern(1, 0, false, true);
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "test");
+        mongoTemplate.setWriteConcern(writeConcern);
+
+        return mongoTemplate;
+    }
+
 }
