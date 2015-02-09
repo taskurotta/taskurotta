d87993fa0f4b3562f1f8e1f668374e1bea5de873
romario13 <romario13@gmail.com>
2015-02-09 11:52:18 +0300

    Add: divide serialization test into 4 independent tests

--- services-hz/src/test/java/ru/taskurotta/hz/test/SerializationCompareTest.java
+++ services-hz/src/test/java/ru/taskurotta/hz/test/SerializationCompareTest.java
@@ -8,6 +8,7 @@ import com.mongodb.DBCollection;
 import org.bson.BSONObject;
 import org.bson.io.BasicOutputBuffer;
 import org.bson.io.OutputBuffer;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,16 +23,15 @@ import ru.taskurotta.service.hz.serialization.TaskContainerStreamSerializer;
 import ru.taskurotta.service.hz.serialization.bson.TaskContainerBSerializer;
 import ru.taskurotta.transport.model.TaskContainer;
 
-import java.io.DataInputStream;
+import java.util.Arrays;
 
-/**
- * Created by greg on 09/02/15.
- */
+@Ignore
 public class SerializationCompareTest {
 
     private final static Logger log = LoggerFactory.getLogger(SerializationCompareTest.class);
     public static final int COUNT = 100000;
 
+    private static int RUN_TESTS = Integer.parseInt("1111", 2);
 
     @Test
     public void test() throws Exception {
@@ -41,28 +41,44 @@ public class SerializationCompareTest {
         final BufferObjectDataOutput objectDataOutput = serializationService.createObjectDataOutput(1024);
 
         TaskContainer taskContainer = BsonSerializationTest.createTaskContainer();
-        byte[] hzArray = null;
-        //hz serialization
-        int objectSize = 0;
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < COUNT; i++) {
-            streamSerializer.write(objectDataOutput, taskContainer);
-            if (hzArray == null) {
-                hzArray = objectDataOutput.toByteArray();
-                objectSize = hzArray.length;
-            }
-            objectDataOutput.clear();
+        long start;
 
+        byte[] byteArray = null;
+
+
+        //hz serialization
+        if ((RUN_TESTS & (1 << 0)) != 0) {
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+                streamSerializer.write(objectDataOutput, taskContainer);
+                if (byteArray == null) {
+                    byteArray = objectDataOutput.toByteArray();
+                }
+                objectDataOutput.clear();
+
+            }
+
+            log.info("HZ: Serialization took {} ms, object size {} bytes", System.currentTimeMillis() - start, byteArray
+                    .length);
         }
-        long serialzationResult = System.currentTimeMillis() - start;
+
+
         //hz deserialization
-        start = System.currentTimeMillis();
-        for (int i = 0; i < COUNT; i++) {
-            BufferObjectDataInput objectDataInput = serializationService.createObjectDataInput(hzArray);
-            TaskContainer taskContainerRestored = streamSerializer.read(objectDataInput);
+        if ((RUN_TESTS & (1 << 1)) != 0) {
+
+            streamSerializer.write(objectDataOutput, taskContainer);
+            byteArray = objectDataOutput.toByteArray();
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+                BufferObjectDataInput objectDataInput = serializationService.createObjectDataInput(byteArray);
+                streamSerializer.read(objectDataInput);
+            }
+
+            log.info("HZ: Deserialization took {} ms object size {} bytes", System.currentTimeMillis() - start, byteArray
+                    .length);
         }
-        long deserialzationResult = System.currentTimeMillis() - start;
-        log.info("HZ: Serialization took {} ms | Deserialization took {} ms | Object size {}", serialzationResult, deserialzationResult, objectSize);
 
 
         final TaskContainerBSerializer serializer = new TaskContainerBSerializer();
@@ -74,32 +90,44 @@ public class SerializationCompareTest {
 
         DBObjectCheat<TaskContainer> dbObjectCheat = new DBObjectCheat<>(taskContainer);
 
-        // bson serialization
-        int bsonObjectSize = 0;
-        start = System.currentTimeMillis();
-        for (int i = 0; i < COUNT; i++) {
-            BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
-            testBEncoder.serialize(basicOutputBuffer, dbObjectCheat);
-            if (bsonObjectSize == 0) {
-                bsonObjectSize = basicOutputBuffer.toByteArray().length;
-            }
-            testBEncoder.done();
-        }
-        long result = System.currentTimeMillis() - start;
+        byteArray = null;
 
-        DBCollection dbCollection = null;
-        DataInputStream in = new DataInputStream(SerializationCompareTest.class.getResourceAsStream("/obj.ser"));
-        byte[] bytes = new byte[1350];
-        in.readFully(bytes);
+
+        // bson serialization
+        if ((RUN_TESTS & (1 << 2)) != 0) {
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+                BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
+                testBEncoder.serialize(basicOutputBuffer, dbObjectCheat);
+                if (byteArray == null) {
+                    byteArray = basicOutputBuffer.toByteArray();
+                }
+                testBEncoder.done();
+            }
+
+            log.info("BSON: Serialization took {} ms, object size {} bytes", System.currentTimeMillis() - start, byteArray
+                    .length);
+        }
+
 
         // bson deserialization
-        start = System.currentTimeMillis();
-        for (int i = 0; i < COUNT; i++) {
-            DBObjectCheat<TaskContainer> objectCheat = (DBObjectCheat) decoder.decode(bytes, dbCollection);
-            TaskContainer container = objectCheat.getObject();
+        if ((RUN_TESTS & (1 << 3)) != 0) {
+
+            BasicOutputBuffer basicOutputBuffer = new BasicOutputBuffer();
+            testBEncoder.serialize(basicOutputBuffer, dbObjectCheat);
+            byteArray = basicOutputBuffer.toByteArray();
+            byteArray = Arrays.copyOfRange(byteArray, 4, byteArray.length);
+
+            start = System.currentTimeMillis();
+            for (int i = 0; i < COUNT; i++) {
+                decoder.decode(byteArray, (DBCollection) null);
+            }
+
+            log.info("BSON: Deserialization took {} ms object size {} bytes", System.currentTimeMillis() - start,
+                    byteArray.length);
         }
-        long deSerializeStop = System.currentTimeMillis() - start;
-        log.info("BSON: Serialization took {} ms | Deserialization took {} ms | Object size {}", result, deSerializeStop, bsonObjectSize);
+
     }
 
 
