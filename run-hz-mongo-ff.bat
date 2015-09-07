c320e20b21cd07521eead6407395ddaa4790547a
stukushin <stukushin@gmail.com>
2015-09-07 15:16:09 +0600

    Change taskurotta version.

--- run-hz-mongo-ff.bat
+++ run-hz-mongo-ff.bat
@@ -1,1 +1,1 @@
-java -server -cp assemble/target/assemble-0.10.0-SNAPSHOT.jar ru.taskurotta.bootstrap.Main -f assemble/src/main/resources/tests/ff-hz-mongo-quantity.yml
+java -server -cp assemble/target/assemble-0.10.4-SNAPSHOT.jar ru.taskurotta.bootstrap.Main -f assemble/src/main/resources/tests/ff-hz-mongo-quantity.yml
