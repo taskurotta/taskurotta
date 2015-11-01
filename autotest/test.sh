a1799d5f8134ec076b5115284f5e094db04798ea
romario13 <romario13@gmail.com>
2015-11-01 09:27:39 +0300

    ./test.sh up - run containers and waiting for the start of the its applications.

--- autotest/test.sh
+++ autotest/test.sh
@@ -43,6 +43,61 @@ do_jar()
     echo ">> done"
 }
 
+# $1 - file path
+# $2 - log string
+f_wait_log_message()
+{
+    while [ ! -f $1 ]
+    do
+      sleep 1
+    done
+
+    if [ -n "$2" ];
+        then grep -q -m 1 "$2" <(tail -f -n 10000 $1)
+    fi
+}
+
+do_up()
+{
+    docker-compose up -d tsk_mongodb
+    echo "Waiting tsk_mongodb..."
+    f_wait_log_message data/mongo/mongodb.log "waiting for connections on port"
+    echo ">> done"
+
+    docker-compose up -d tsk_node1
+    echo "Waiting tsk_node1..."
+    f_wait_log_message data/node1/service.log "Members \[1\] {"
+    echo ">> done. Member is started"
+    f_wait_log_message data/node1/service.log "POST    /tasks/gc"
+    echo ">> done. And ready for connections"
+
+    docker-compose up -d tsk_node2
+    echo "Waiting tsk_node2..."
+    f_wait_log_message data/node1/service.log "Members \[2\] {"
+    echo ">> done. Member is started and connected to cluster"
+    f_wait_log_message data/node1/service.log "POST    /tasks/gc"
+    echo ">> done. And ready for connections"
+
+    docker-compose up -d tsk_nginx
+    echo "Waiting tsk_nginx..."
+    f_wait_log_message data/nginx/access.log
+    echo ">> done."
+
+    docker-compose up -d tsk_actors
+    echo "Waiting tsk_actors..."
+    f_wait_log_message data/actors/actors.log "Bootstrap - \[2\] actors started..."
+    echo ">> done."
+
+    docker-compose up -d tsk_pusher
+    echo "Waiting tsk_pusher..."
+    f_wait_log_message data/pusher/pusher.log "ProcessPusher - process pusher"
+    echo ">> done."
+}
+
+do_stop()
+{
+    docker-compose stop
+}
 
 for var in "$@"
 do
@@ -63,8 +118,16 @@ do
         jar)
             do_jar
             ;;
+        up)
+            do_clean_docker
+            do_clean_data
+            do_up
+        ;;
+        stop)
+            do_stop
+        ;;
         *)
-            echo "Usage: test {build|clean-data|clean-docker|clean|jar}" >&2
+            echo "Usage: test {build|clean-data|clean-docker|clean|jar|up|stop}" >&2
             exit 3
         ;;
 
