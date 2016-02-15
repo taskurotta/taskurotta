eb9beb168e121c50c35bfb21887f7659ce5a5239
romario13 <romario13@gmail.com>
2016-02-15 16:46:53 +0300

    e2e test fix

--- dropwizard/src/main/e2e/interrupted_tsk_list.js
+++ dropwizard/src/main/e2e/interrupted_tsk_list.js
@@ -3,7 +3,7 @@ describe('taskurotta interrupted task groups', function() {
 
     it('should list tasks', function() {
         browser.get('http://tsk_http/app.html');
-        element(by.linkText("Tasks2")).click();
+        element(by.linkText("Tasks")).click();
         element(by.linkText("Interrupted tasks groups...")).click();
     });
 });
