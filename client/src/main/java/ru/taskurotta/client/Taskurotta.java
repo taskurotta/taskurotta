17a4a97e24051f575aa33b925badf6dbf0f55e19
romario13 <romario13@gmail.com>
2016-03-17 03:17:35 +0300

    Simple process start style added

---
+++ client/src/main/java/ru/taskurotta/client/Taskurotta.java
@@ -0,0 +1,34 @@
+package ru.taskurotta.client;
+
+import ru.taskurotta.client.internal.CommonClientServiceManager;
+import ru.taskurotta.client.jersey.JerseyHttpTaskServerProxy;
+
+/**
+ */
+public class Taskurotta {
+
+    DeciderClientProvider clientProvider;
+
+    public Taskurotta(String endpoint) {
+        this(endpoint, 1, 3000, 0);
+    }
+
+    public Taskurotta(String endpoint, int threadPoolSize, long connectionTimeout, long readTimeout) {
+
+        JerseyHttpTaskServerProxy taskServer = new JerseyHttpTaskServerProxy();
+        taskServer.setConnectTimeout(connectionTimeout);
+        taskServer.setReadTimeout(readTimeout);
+        taskServer.setThreadPoolSize(threadPoolSize);
+        taskServer.setEndpoint(endpoint);
+        taskServer.setMaxConnectionsPerHost(threadPoolSize);
+        taskServer.init();
+
+        ClientServiceManager clientServiceManager = new CommonClientServiceManager(taskServer);
+
+        this.clientProvider = clientServiceManager.getDeciderClientProvider();
+    }
+
+    public <T> T createDecider(Class<T> clazz) {
+        return clientProvider.getDeciderClient(clazz);
+    }
+}
